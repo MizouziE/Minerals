2b7d5a2d2f43067189534a1fc098cf167b96c61f
MizouziE <90829439+MizouziE@users.noreply.github.com>
2022-03-18 16:50:02 +0000

    best meal & recommended section on meals page

--- resources/views/meals/index.blade.php
+++ resources/views/meals/index.blade.php
@@ -21,8 +21,28 @@
             </a>
             <div class="text-gray-400 mt-1">Meal-type</div>
         </div>
+    </div><!-- end list of meals -->
+    <div class="flex my-10">
+        <div class="best-meal w-3/4 mr-32">
+            <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Best Meal</h2>
+            <div class="best-meal-container space-y-12 mt-8">
+                <div class="meal bg-gray-800 rounded-lg shadow-md flex px-6 py-6">
+                    <div class="relative flex-none">
+                        <a href="#">
+                            <img src="{{ asset('images/micronutrients.jpg') }}" alt="Meal" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                        </a>
+                        <div class="absolute bottom-0 right-0 h-12 w-12 bg-gray-900 rounded-full" style="right: -20px; bottom: -20px;">
+                            <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="recommended w-1/4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum dolorum officia vitae enim, unde obcaecati animi non tempora amet veniam dignissimos. Mollitia in cum expedita adipisci, quasi, vero neque impedit quas aliquid molestias recusandae ratione repudiandae tempore quae nostrum dolorem vel harum, explicabo nesciunt? Est nisi nam quae unde beatae! Unde vero deleniti magnam impedit magni repudiandae blanditiis, veritatis officia?
+        </div>
     </div>
+</div><!-- end container -->
 
-</div>
 
 @endsection

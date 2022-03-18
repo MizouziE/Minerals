de21a5a02bf23cddfba6c49d2b8ab83a32fc2eac
MizouziE <90829439+MizouziE@users.noreply.github.com>
2022-03-18 11:44:03 +0000

    making meal page template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,15 +12,33 @@
         <nav class="container mx-auto flex items-center justify-between px-4 py-6">
             <div class="flex items-center">
                 <a href="/">
-                    <img src="../../../public/images/logo.png" alt="Minerals" class="w-32 flex-none">
+                    <img src="{{ asset('images/logo.svg') }}" alt="Minerals" class="w-32 flex-none">
                 </a>
-                <ul class="flex ml-16 space-x-8">
+                <ul class="flex ml-16 space-x-5">
                     <li><a href="#" class="hover:text-gray-400">Dashboard</a></li>
                     <li><a href="#" class="hover:text-gray-400">Food</a></li>
                     <li><a href="#" class="hover:text-gray-400">Suggestions</a></li>
                 </ul>
             </div>
+            <div class="flex items-center">
+                <div class="relative">
+                    <input type="text" class="bg-gray-800 text-sm rounded-full w-64 focus:outline-none focus:shadow-outline px-3 py-1" placeholder="Search...">
+                </div>
+                <div class="ml-6">
+                    <a href="#"><img src="{{ asset('images/orange-slice.png') }}" alt="orange" class="rounded-full w-16 h-16"></a>
+                </div>
+            </div>
         </nav>
     </header>
+
+    <main class="py-8">
+        @yield('content')
+    </main>
+
+    <footer class="border-t text-xs border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            Powered By <a href="https://fdc.nal.usda.gov" target="_blank" class="underline hover:text-gray-400">U.S. Department of Agriculture, Agricultural Research Service. FoodData Central, 2019.</a>
+        </div>
+    </footer>
 </body>
 </html>

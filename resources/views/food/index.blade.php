bf592ada1031e4f974485f2d643177ac2c25191c
MizouziE <90829439+MizouziE@users.noreply.github.com>
2022-03-19 15:13:10 +0000

    rounded image corners on index pages

--- resources/views/food/index.blade.php
+++ resources/views/food/index.blade.php
@@ -12,7 +12,7 @@
         <div class="food mt-8">
             <div class="relative inline-block">
                 <a href="#">
-                    <img src="{{ asset('images/apple.jpg') }}" alt="food" class="hover:opacity-75 transition ease-in-out duration-150">
+                    <img src="{{ asset('images/apple.jpg') }}" alt="food" class="hover:opacity-75 rounded-lg transition ease-in-out duration-150">
                 </a>
                 <div class="absolute bottom-0 right-0 h-12 w-12 bg-gray-800 rounded-full" style="right: -20px; bottom: -20px;">
                     <div class="font-semibold text-xs flex justify-center items-center h-full">80%</div>

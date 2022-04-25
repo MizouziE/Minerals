5bdbca9168a800b00428982333b0ba17026387bc
MizouziE <90829439+MizouziE@users.noreply.github.com>
2022-04-25 17:53:14 +0000

    added og:image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:image" content="https://micro-nutri-track.herokuapp.com//images/micronutrients.jpg"/>
+    <meta property="og:image:width" content="1000"/>
+    <meta property="og:image:height" content="667"/>
     <title>Minerals</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
